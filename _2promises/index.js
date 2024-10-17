5c1619759ded085277a9bc4ef7f11ca1be369d4f
amitesh0303 <amiteshpatnaik.cse.2019@nist.edu>
2024-10-17 04:25:04 +0000

    added class example

--- _2promises/index.js
+++ _2promises/index.js
@@ -1,78 +1,23 @@
-// Example 1: Basic Promise
-const basicPromise = new Promise((resolve, reject) => {
-    let success = true; // Change this to false to see the rejection case
-    if (success) {
-        resolve("Promise resolved successfully!");
-    } else {
-        reject("Promise rejected!");
+//Classes
+class Rectangle{
+    constructor(height,width,color){
+        this.height=height;
+        this.width=width;
+        this.color=color;
     }
-});
+    area(){
+        const area=this.height*this.width;
+        return area;
+    }
 
-basicPromise
-    .then((message) => {
-        console.log(message);
-    })
-    .catch((error) => {
-        console.error(error);
-    });
+    paint(){
+        console.log(`The color of rect is ${this.color}`);
+    }
+}
 
-// Example 2: Promise with setTimeout
-const delayedPromise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        resolve("Delayed promise resolved after 2 seconds!");
-    }, 2000);
-});
+const rect=new Rectangle(3,4)
+let area=rect.area();
+console.log(area);
 
-delayedPromise
-    .then((message) => {
-        console.log(message);
-    })
-    .catch((error) => {
-        console.error(error);
-    });
+//Inheritance
 
-// Example 3: Chaining Promises
-const chainedPromise = new Promise((resolve, reject) => {
-    resolve(10);
-});
-
-chainedPromise
-    .then((result) => {
-        console.log(result); // 10
-        return result * 2;
-    })
-    .then((result) => {
-        console.log(result); // 20
-        return result * 3;
-    })
-    .then((result) => {
-        console.log(result); // 60
-    })
-    .catch((error) => {
-        console.error(error);
-    });
-
-// Example 4: Promise.all
-const promise1 = Promise.resolve(3);
-const promise2 = 42;
-const promise3 = new Promise((resolve, reject) => {
-    setTimeout(resolve, 100, 'foo');
-});
-
-Promise.all([promise1, promise2, promise3])
-    .then((values) => {
-        console.log(values); // [3, 42, "foo"]
-    });
-
-// Example 5: Promise.race
-const promiseA = new Promise((resolve, reject) => {
-    setTimeout(resolve, 500, 'A');
-});
-const promiseB = new Promise((resolve, reject) => {
-    setTimeout(resolve, 100, 'B');
-});
-
-Promise.race([promiseA, promiseB])
-    .then((value) => {
-        console.log(value); // "B" - the fastest promise
-    });
